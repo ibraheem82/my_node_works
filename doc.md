d1b72ca936b9ffd80fdcbfb621c13c62fe69d38b
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-07-29 13:22:12 +0100

    finished the node modules lessons

--- doc.md
+++ doc.md
@@ -1,3 +1,5 @@
 # You need to explicitly tell node to use your file as an ecma script module, by naming your file the mjs extension [.mjs] the [m] is for module.
 # Node treats javaScript file commonJs module by default for backward compatibility.
 # The file extension is always neccessary when importing or with import the file which is [.mjs].
+
+# It is not a must you use [index.js], but you need to be more familiar with it.
--- internals/index.js
+++ internals/index.js
@@ -1,6 +1,23 @@
-const { request } = require("http");
+// ! You dont need this when using the spread operator
+// const request = require('./request');
+// const response = require('./response');
 
-module.exports = { 
-    request: require('./request.js'),
-    response: require('./response.js'),
+// module.exports = { 
+//     request: require('./request.js'),
+//     response: require('./response.js'),
+// }
+
+
+// * Using spread operator
+module.exports = {
+    ...require('./request'),
+    ...require('./response'),
 }
+
+
+
+// module.exports = {
+//     REQUEST_TIMEOUT: request.REQUEST_TIMEOUT,
+//     send: request.send,
+//     read: response.read,
+// }
